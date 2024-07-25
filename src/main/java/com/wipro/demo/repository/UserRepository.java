1a86f1ff7e2bf27acf9a2ac4294baa8740a2d5fb
ssarun9871 <ssarun9871@gmail.com>
2024-07-25 13:26:39 +0530

    update

--- src/main/java/com/wipro/demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.wipro.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.wipro.demo.entity.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    User findByUsername(String username);
-}
-
